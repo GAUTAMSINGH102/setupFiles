ca2835cf647301046faabbc66f9e145b74e5b999
Gautam Singh <gautamsingh.00721@gmail.com>
2024-01-21 16:16:21 +0530

    ClientDB

---
+++ dummyUpdated/main.py
@@ -0,0 +1,517 @@
+all = """
+"**BEGIN,2022:P:USDECORW:1,85-3349177,Default,Default,Main Office/Main"
+\@101 \ 1
+\:1
+\&1
+.40 US Decor Wonderland LLC
+.43 297 Engle St
+.44 Tenafly
+.45 NJ
+.46 07670
+.91 Furniture
+.90 Home Furnishing
+.92 423200
+.93 10/07/2020
+.42 85-3349177
+.30 NJ
+.96 C
+.95 daniel.kalina@decorwonderland.com
+.80 B
+.200M 2021
+.201M X
+.202M 2021.0503
+.204M X
+.205M USDECORW:V1
+.206M 2021.05030
+.207M 2021.04021
+.210M 115274
+\@103 \ 3
+\:1
+\&1
+.40 1
+.90 State Code blank or invalid. Review Partner Information. Daniel/Kalina
+.92 State Code blank or invalid. Review Partner Information. Daniel/Kalina
+.94 State Code blank or invalid. Review Partner Information. Karen/Kalina
+.96 State Code blank or invalid. Review Partner Information. Karen/Kalina
+.98 Business Code blank or invalid. Review  Review Basic Data.
+.100 Business Start Date invalid.  Review Basic Data.
+.102 End of Year Partner Capital Account missing. Review Partner Information.
+\@103 \ 3
+\:1
+\&2
+.90 End of Year Partner Capital Account missing. Review Partner Information.
+.92 Pro Forma Alert:  Client address has been transferred from the
+.94 prior year. Please review to ensure the information is correct.
+.96 Pro Forma Alert:  Overrides have been transferred from the prior year to
+.98 Federal > General > Basic Data > Other Information section
+.100 (Interview Form 4). Please verify if these still apply.
+.291M X
+.292M X
+.293M X
+.294M X
+.295M X
+.93 X
+.95 X
+.97 X
+.99 X
+.101 X
+\@104 \ 4
+\:1
+\&1
+.50 3
+.57 N
+.61 B
+\@743 \ QBI-1
+\:1
+\&1
+.40 TB
+.41 1
+.42 X
+\@641 \ EF-2
+\:1
+\&1
+.60 12345
+.62 2
+.63 Daniel Kalina
+.65 Member
+.66 092-68-4142
+\@225 \ EF-4
+\:1
+\&1
+.30 367780
+\@120 \ 2YR-1
+\:1
+\&1
+.40 440655
+.43 219034
+.47 219034
+.48 146159
+.49 72875
+.50 367780
+.55 367780
+.71 43511
+.72 43511
+.73 324269
+.32 2
+\@121 \ 2YR-2
+\:1
+\&1
+.40 324269
+.80 324269
+.82 367780
+\@122 \ 2YR-3
+\:1
+\&1
+.64 324269
+.71 324269
+.77 324269
+.83 324269
+.91 181259
+.92 324269
+.94 505528
+.99 505528
+\@131 \ A-1
+\:1
+\&1
+.30 Partnership Activity
+".40M 440,655."
+\@132 \ A-2
+\:1
+\&1
+.30 146159
+".35M 219,034."
+".157M 146,159."
+\@134 \ A-4
+\:1
+\&1
+.32 Agent Fee
+.38 Temporary Staffing
+.44 Insurance
+.50 Listing Fees
+.33M 4.
+".39M 42,058."
+".45M 1,442."
+.51M 7.
+\@271 \ K-1
+\:1
+\&1
+.30 1
+.37 2
+.31 Daniel
+.38 Karen
+.109 247709
+.58 297 Engle Street
+.59 Tenafly
+.62 297 Engle Street
+.63 Tenafly
+.134 505528
+.60 NJ
+.102 092-68-4142
+.165 X
+.103 I
+.64 NJ
+.110 066-76-0542
+.178 X
+.111 I
+.117 257819
+.106 .49
+.107 .49
+.108 .49
+.114 .51
+.115 .51
+.116 .51
+.33 Kalina
+.40 Kalina
+.61 07670
+.65 07670
+\@272 \ K-2
+\:1
+\&1
+.50 1
+.54 2
+\@291 \ L-1
+\:1
+\&1
+.35 252455
+.37 119188
+.43 146159
+.114 Escrow
+.116 10000
+.LIST 116 _ 2
+BCHOI01 - 09/10/22 16:13 PM Worksheet Balance Sheet
+Sunlight Escrow                           _       10000
+.130 527802
+\@292 \ L-2
+\:1
+\&1
+.30 22274
+.157 527802
+.118 505528
+\@293 \ L-3
+\:1
+\&1
+".80M 181,259."
+\@379 \ EXT-1
+\:1
+\&1
+.30 2
+\@388 \ EXT-2
+\:1
+\&1
+.30 X
+\@6697 \ NJ8
+\:1
+\&1
+.84 324269
+\*
+"**BEGIN,2022:C:AEBCINC:1,45-3525969,Default,Default,Main Office/Main"
+\@101 \ 1
+\:1
+\&1
+.40 All Empire Building Contractors Inc
+.42 45-3525969
+.43 60 Allen St
+.44 New Hyde Park
+.45 NY
+.46 11040
+.48 212-920-4870
+.49 President
+.50 09/01/22
+.51 08/31/23
+.52 09/27/2011
+.53 236110
+.54 Construction
+.55 Construction
+.56 Ny
+.60 allempire247@gmail.com
+.75 Y
+.201M X
+.202M 2021.0503
+.204M X
+.205M AEBCINC:V1
+.206M 2021.05030
+.207M 2021.04014
+.210M 115274
+\@103 \ 3
+\:1
+\&1
+.40 1
+.90 Pro Forma Alert:  Client address has been transferred from the prior
+.92 year.  Please review to ensure the information is correct.
+.91 X
+.290M X
+.291M X
+.93 X
+\@104 \ 4
+\:1
+\&1
+.38 N
+.50 C
+.55 N
+.61 N
+\@921 \ BNK-1
+\:1
+\&1
+.36 021000322
+.37 483075836541
+.42 1
+.38 1
+\@590 \ OS-1
+\:1
+\&1
+.30 1
+.31 Harjinder
+.33 Singh
+.34 60 Allen St
+.78 612-08-8995
+.80 1
+.50 New Hyde Park
+.51 NY
+.52 11040
+\@591 \ OS-2
+\:1
+\&1
+.30 1
+.47 1
+.40 US
+\@300 \ EF-2
+\:1
+\&1
+.63 Harjinder Singh
+\@132 \ A-2
+\:1
+\&1
+.119 Other Construction Costs
+\@133 \ A-3
+\:1
+\&1
+.125 Licenses and permits
+.134 Payroll Taxes
+.143 Payroll Taxes
+\@134 \ A-4
+\:1
+\&1
+.34 Accounting
+.41 Auto and Truck Expense
+.48 Bank Charges
+.55 Insurance
+.62 Legal and Professional
+.69 Miscellaneous
+.76 Office Expense
+.83 Outside Service
+.90 Parking and Tolls
+.97 Postage
+.104 Supplies
+.111 Telephone
+.118 Travel
+\@610 \ L-10
+\:1
+\&1
+.44 N
+.45 N
+.48 N
+.49 N
+.50 N
+.51 N
+\@351 \ P-1
+\:1
+\&1
+.96M 12/15/2022
+.97M 02/15/2023
+\*
+"**BEGIN,2022:C:AEBCINC:1,45-3525969,Default,Default,Main Office/Main"
+\@101 \ 1
+\:1
+\&1
+.40 All Empire Building Contractors Inc
+.42 45-3525969
+.43 60 Allen St
+.44 New Hyde Park
+.45 NY
+.46 11040
+.48 212-920-4870
+.49 President
+.50 09/01/22
+.51 08/31/23
+.52 09/27/2011
+.53 236110
+.54 Construction
+.55 Construction
+.56 Ny
+.60 allempire247@gmail.com
+.75 Y
+.201M X
+.202M 2021.0503
+.204M X
+.205M AEBCINC:V1
+.206M 2021.05030
+.207M 2021.04014
+.210M 115274
+\@103 \ 3
+\:1
+\&1
+.40 1
+.90 Pro Forma Alert:  Client address has been transferred from the prior
+.92 year.  Please review to ensure the information is correct.
+.91 X
+.290M X
+.291M X
+.93 X
+\@104 \ 4
+\:1
+\&1
+.38 N
+.50 C
+.55 N
+.61 N
+\@921 \ BNK-1
+\:1
+\&1
+.36 021000322
+.37 483075836541
+.42 1
+.38 1
+\@590 \ OS-1
+\:1
+\&1
+.30 1
+.31 Harjinder
+.33 Singh
+.34 60 Allen St
+.78 612-08-8995
+.80 1
+.50 New Hyde Park
+.51 NY
+.52 11040
+\@591 \ OS-2
+\:1
+\&1
+.30 1
+.47 1
+.40 US
+\@300 \ EF-2
+\:1
+\&1
+.63 Harjinder Singh
+\@132 \ A-2
+\:1
+\&1
+.119 Other Construction Costs
+\@133 \ A-3
+\:1
+\&1
+.125 Licenses and permits
+.134 Payroll Taxes
+.143 Payroll Taxes
+\@134 \ A-4
+\:1
+\&1
+.34 Accounting
+.41 Auto and Truck Expense
+.48 Bank Charges
+.55 Insurance
+.62 Legal and Professional
+.69 Miscellaneous
+.76 Office Expense
+.83 Outside Service
+.90 Parking and Tolls
+.97 Postage
+.104 Supplies
+.111 Telephone
+.118 Travel
+\@610 \ L-10
+\:1
+\&1
+.44 N
+.45 N
+.48 N
+.49 N
+.50 N
+.51 N
+\@351 \ P-1
+\:1
+\&1
+.96M 12/15/2022
+.97M 02/15/2023
+\*
+"""
+
+
+import pandas as pd
+
+lastfinal = {}
+final = {}
+data = {}
+prev = ''
+count = 1
+
+for line in all.split("\n"):
+    if line.startswith('\"**BEGIN') or line.startswith('**BEGIN'):
+      # print(line)
+      lastfinal[count] = {}
+
+      # Split the input string by ':' to get an array of substrings
+      substr_arr = line.split(':')
+
+      # Extract the required information from the substr_arr using array indexing
+      year = substr_arr[0].split(',')[1]   # '2022'
+      returnType = substr_arr[1]               # 'I'
+      name = substr_arr[2]                 # 'BASSEYJ'
+      ssn = substr_arr[3]                  # '1,123-45-6789'
+
+      # Extract only the SSN from the substr_arr using string manipulation
+      identifier = ssn.split(',')[1]              # '123-45-6789'
+
+      # The extracted information
+      final['year'] = year
+      final['returnType'] = returnType
+      final['name'] = name
+      final['identifier'] = identifier
+
+    if line.startswith("\@"):
+      # arr = line.split(" ")
+      # first = arr[0]
+      first = line
+      final[prev] = data
+      prev = first
+      data = {}
+
+    if line.startswith("\*"):
+      arr = line.split(" ")
+      first = arr[0]
+      final[prev] = data
+      prev = ''
+      data = {}
+      lastfinal[count] = final
+      count = count + 1
+      final = {}
+
+    if line.startswith(".") or line.startswith('\".'):
+        column_name, value = line.split(" ", 1)
+        data[column_name] = value
+
+
+for key, value in lastfinal.items():
+  print(value['year'])
+
+def flatten_dict(data, parent_key='', sep='.'):
+    items = []
+    for k, v in data.items():
+        new_key = f"{parent_key}{sep}{k}" if parent_key else k
+        if isinstance(v, dict):
+            items.extend(flatten_dict(v, new_key, sep=sep).items())
+        else:
+            items.append((new_key, v))
+    return dict(items)
+
+# Flatten the nested dictionary
+flattened_data = {}
+for key, value in lastfinal.items():
+    flattened_data[key] = flatten_dict(value)
+
+# df = pd.DataFrame.from_dict(lastfinal)
+df = pd.DataFrame.from_dict(flattened_data, orient='index')
+
+# Save the dataframe to an Excel file
+df.to_excel('third.xlsx', index=False)
+
